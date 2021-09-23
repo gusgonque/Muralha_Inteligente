f34d9407ce4d30a74e6f44172c60639036d433ad
Gustavo <gustavoguguqueiroz@hotmail.com>
2021-09-23 11:19:01 -0300

    Projeto Muralha Inteligente

--- lib/screens/car_add.dart
+++ lib/screens/car_add.dart
@@ -2,15 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CarAdd extends StatefulWidget {
-
   @override
   _CarAddState createState() => _CarAddState();
 }
 
 class _CarAddState extends State<CarAdd> {
   final TextEditingController _placaVeiculo = TextEditingController();
-
   final TextEditingController _descricaoVeiculo = TextEditingController();
+  final TextEditingController _localVeiculo = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -20,14 +19,19 @@ class _CarAddState extends State<CarAdd> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
+        child: ListView(
           children: <Widget>[
-            TextField(
-              controller: _placaVeiculo,
-              decoration: InputDecoration(
-                labelText: 'Placa',
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                controller: _placaVeiculo,
+                decoration: InputDecoration(
+                  labelText: 'Placa',
+                ),
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
               ),
-              style: TextStyle(fontSize: 24.0),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 8.0),
@@ -40,16 +44,29 @@ class _CarAddState extends State<CarAdd> {
               ),
             ),
             Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                controller: _localVeiculo,
+                decoration: InputDecoration(
+                  labelText: 'Localização',
+                ),
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+            Padding(
               padding: const EdgeInsets.only(top: 16.0),
               child: SizedBox(
                 width: double.maxFinite,
-                child:
-                    ElevatedButton(onPressed: () {
+                child: ElevatedButton(
+                    onPressed: () {
                       final String placa = _placaVeiculo.text;
                       final String descricao = _descricaoVeiculo.text;
-                    }, child: Text('Adicionar')),
+                      final String local = _localVeiculo.text;
+                      //return Car(placa,descricao,local);
+                    },
+                    child: Text('Adicionar')),
               ),
-            )
+            ),
           ],
         ),
       ),
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:muralha_inteligente_app/screens/car_add.dart';
 import 'package:muralha_inteligente_app/screens/car_list.dart';
 
 class Dashboard extends StatelessWidget {
@@ -18,43 +19,80 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('lib/images/logo.png'),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Theme.of(context).primaryColor,
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                    (MaterialPageRoute(
-                      builder: (context) => CarList(),
-                    )),
-                  );
+          Row(
+            children: [
+              _FeatureItem(
+                'Lista de\nVeículos suspeitos',
+                Icons.list,
+                onClick: () {
+                  _showCarList(context);
                 },
-                child: Container(
-                  padding: EdgeInsets.all(8.0),
-                  height: 120,
-                  width: 180,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(
-                        Icons.list,
-                        size: 30,
-                        color: Colors.white,
-                      ),
-                      Text('Lista de\nVeículos suspeitos',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                          )),
-                    ],
-                  ),
-                ),
               ),
-            ),
-          )
+              _FeatureItem('Adicionar\nVeículo suspeito', Icons.add,
+                  onClick: () {
+                _showCarAdd(context);
+              }),
+            ],
+          ),
         ],
+      ),
+    );
+  }
+}
+
+void _showCarList(BuildContext context) {
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (context) => CarList(),
+    ),
+  );
+}
+
+void _showCarAdd(BuildContext context) {
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (context) => CarAdd(),
+    ),
+  );
+}
+
+class _FeatureItem extends StatelessWidget {
+  final String name;
+  final IconData icon;
+  final Function onClick;
+
+  _FeatureItem(this.name, this.icon, {required this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () => onClick(),
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            height: 120,
+            width: 180,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icon,
+                  size: 30,
+                  color: Colors.white,
+                ),
+                Text(name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    )),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
