b6a709d41c245e3fbc39776406c7cbd86fb02397
Lab_Iot <gustavoguguqueiroz@hotmail.com>
2022-02-21 13:47:59 -0300

    21022022 2

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -17,21 +17,21 @@ class Map extends StatelessWidget {
       ),
       body: FlutterMap(
         options: MapOptions(
-          center: LatLng(0, 0),
-          zoom: 1,
+          center: LatLng(-14.2400732,-53.1805017), // coordernadas do Brasil
+          zoom: 3.5,
           maxZoom: 19,
         ),
         layers: [
           MarkerLayerOptions(
             markers: [
               new Marker(
-                width: 10.0,
-                height: 10.0,
+                width: 20.0,
+                height: 20.0,
                 point: new LatLng(_latitude, _longitude),
                 builder: (ctx) =>
                   new Container(
                     child: Image.asset('lib/images/carro.png'),
-                    //TODO: Fazer um widget que faz o ícone do carro.
+                    //TODO: Fazer um widget que faz o ícone do carro. ?
                   ),
               ),
             ],
@@ -50,7 +50,7 @@ class Map extends StatelessWidget {
               marker: const DefaultLocationMarker(
                 color: Colors.blue,
               ),
-              markerSize: const Size(10, 10),
+              markerSize: const Size(15, 15),
               accuracyCircleColor: Colors.blue.withOpacity(0.1),
               headingSectorColor: Colors.blue.withOpacity(0.8),
               headingSectorRadius: 100,
