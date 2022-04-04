51d49c441460328181a308e1064e0d22b0782618
Lab_Iot <gustavoguguqueiroz@hotmail.com>
2022-04-04 14:24:48 -0300

    04/04/2022 Muralha-Inteligente 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:muralha_inteligente_app/screens/dashboard.dart';
 
-void main() => runApp(MuralhaInteligenteApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  String? token = await messaging.getToken();
+  print('token = $token');
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    print('User granted permission');
+  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    print('User granted provisional permission');
+  } else {
+    print('User declined or has not accepted permission');
+  }
+
+  runApp(MuralhaInteligenteApp());
+}
 class MuralhaInteligenteApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
