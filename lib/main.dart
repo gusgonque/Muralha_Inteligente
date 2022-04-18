3e5b6764a3e67d88da63169bb0483eadd0b586c7
Lab_Iot <gustavoguguqueiroz@hotmail.com>
2022-04-18 09:51:13 -0300

    13/04/2022 Muralha-Inteligente 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:muralha_inteligente_app/screens/dashboard.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
-class LocalNotificationService {
-  static final FlutterLocalNotificationsPlugin _notificationsPlugin = FlutterLocalNotificationsPlugin();
-
-  static void initialize(BuildContext context) {
-    final InitializationSettings initializationSettings = InitializationSettings(
-        android: const AndroidInitializationSettings("@mipmap/your_icon"));
-
-    _notificationsPlugin.initialize(initializationSettings);
-  }
-
-
-//=================================================
-//==============this is the update notification
-
-  static Future<void> showProgressNotification() async {
-    const int maxProgress = 5;
-    for (int i = 0; i <= maxProgress; i++) {
-      await Future<void>.delayed(const Duration(seconds: 1), () async {
-        final AndroidNotificationDetails androidPlatformChannelSpecifics =
-        AndroidNotificationDetails('progress channel', 'progress channel',
-            channelDescription: 'progress channel description',
-            channelShowBadge: false,
-            importance: Importance.max,
-            priority: Priority.high,
-            playSound: false,
-            showProgress: true,
-            maxProgress: maxProgress,
-            progress: i);
-        final NotificationDetails platformChannelSpecifics =
-        NotificationDetails(android: androidPlatformChannelSpecifics);
-        await _notificationsPlugin.show(
-            0,//I use this id to cancel it from below method
-            'progress notification title',
-            'progress notification body',
-            platformChannelSpecifics,
-            payload: 'item x');
-      });
-    }
-  }
-
-  //=========================and this is for the ProgressNotification to be cancelled
-  static Future<void> cancelNotification() async {
-    await _notificationsPlugin.cancel(0);
-  }
-
-}
+import 'package:muralha_inteligente_app/screens/dashboard.dart';
+import 'notifications/notifications.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
+
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -67,32 +21,26 @@ void main() async{
     carPlay: false,
     criticalAlert: false,
     provisional: false,
-    sound: true,
+    sound: false,
   );
-
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-  FlutterLocalNotificationsPlugin();
-// initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
-  const AndroidInitializationSettings initializationSettingsAndroid =
-  AndroidInitializationSettings('app_icon');
-  final IOSInitializationSettings initializationSettingsIOS =
-  IOSInitializationSettings(
-      onDidReceiveLocalNotification: onDidReceiveLocalNotification);
-  final MacOSInitializationSettings initializationSettingsMacOS =
-  MacOSInitializationSettings();
-  final InitializationSettings initializationSettings = InitializationSettings(
-      android: initializationSettingsAndroid,
-      iOS: initializationSettingsIOS,
-      macOS: initializationSettingsMacOS);
-  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
-      onSelectNotification: selectNotification);
 
   if (settings.authorizationStatus == AuthorizationStatus.authorized || settings.authorizationStatus == AuthorizationStatus.provisional) {
     print('User granted permission');
-    _startNotificationHandler(messaging);
+    startNotificationHandler(messaging);
   } else {
     print('User declined or has not accepted permission');
   }
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String? payload) async {
+        if (payload != null) {
+          debugPrint('notification payload: $payload');
+        }
+        selectedNotificationPayload = payload;
+        selectNotificationSubject.add(payload);
+      },
+  );
+
   runApp(MuralhaInteligenteApp());
 }
 
@@ -111,32 +59,5 @@ class MuralhaInteligenteApp extends StatelessWidget{
       home: Dashboard(),
       navigatorKey: navigatorKey,
     );
-  }
-}
-
-Future<void> _startNotificationHandler(FirebaseMessaging messaging) async {
-  String? token = await messaging.getToken();
-  print('token = $token');
-
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    print ('mensagem recebida em foreground');
-  });
-
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-}
-
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  print("Mensagem recebida em background"); // : ${message.data}
-  Position? position = await Geolocator.getLastKnownPosition();
-
-  double distance = Geolocator.distanceBetween(position?.latitude ?? 0, position?.longitude ?? 0, double.parse(message.data["lat"]), double.parse(message.data["long"]));
-  var distanceInKm = (distance / 1000);
-
-  print('Distance is: $distanceInKm');
-  if (distanceInKm <= 5) {
-    print("NOTIFICAR.");
-  } else {
-    print("NÃO NOTIFICAR.");
-    await LocalNotificationService.cancelNotification();
   }
 }
---
+++ lib/notifications/notifications.dart
@@ -0,0 +1,104 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:geolocator/geolocator.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
+
+String? selectedNotificationPayload;
+
+final BehaviorSubject<ReceivedNotification> didReceiveLocalNotificationSubject =
+BehaviorSubject<ReceivedNotification>();
+
+final BehaviorSubject<String?> selectNotificationSubject =
+BehaviorSubject<String?>();
+
+class LocalNotificationService {
+  static final FlutterLocalNotificationsPlugin _notificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  static void initialize(BuildContext context) {
+    final InitializationSettings initializationSettings = InitializationSettings(
+        android: const AndroidInitializationSettings("@mipmap/your_icon"));
+
+    _notificationsPlugin.initialize(initializationSettings);
+  }
+
+//=================================================
+//==============this is the update notification
+
+  static Future<void> showProgressNotification() async {
+    const int maxProgress = 5;
+    for (int i = 0; i <= maxProgress; i++) {
+      await Future<void>.delayed(const Duration(seconds: 1), () async {
+        final AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails('progress channel', 'progress channel',
+            channelDescription: 'progress channel description',
+            channelShowBadge: false,
+            importance: Importance.max,
+            priority: Priority.high,
+            playSound: false,
+            showProgress: true,
+            maxProgress: maxProgress,
+            progress: i);
+        final NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+        await _notificationsPlugin.show(
+            0,//I use this id to cancel it from below method
+            'progress notification title',
+            'progress notification body',
+            platformChannelSpecifics,
+            payload: 'item x');
+      });
+    }
+  }
+
+  //=========================and this is for the ProgressNotification to be cancelled
+  static Future<void> cancelNotification() async {
+    await _notificationsPlugin.cancel(0);
+  }
+
+}
+
+class ReceivedNotification {
+  ReceivedNotification({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.payload,
+  });
+
+  final int id;
+  final String? title;
+  final String? body;
+  final String? payload;
+}
+
+Future<void> startNotificationHandler(FirebaseMessaging messaging) async {
+  String? token = await messaging.getToken();
+  print('token = $token');
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print ('mensagem recebida em foreground');
+  });
+
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+}
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Mensagem recebida em background"); // : ${message.data}
+  Position? position = await Geolocator.getLastKnownPosition();
+
+  double distance = Geolocator.distanceBetween(position?.latitude ?? 0, position?.longitude ?? 0, double.parse(message.data["lat"]), double.parse(message.data["long"]));
+  var distanceInKm = (distance / 1000);
+
+  print('Distance is: $distanceInKm');
+  if (distanceInKm <= 5) {
+    print("NOTIFICAR.");
+  } else {
+    print("NÃO NOTIFICAR.");
+    await flutterLocalNotificationsPlugin.initialize();
+
+    await flutterLocalNotificationsPlugin.cancel(int.parse(message.data["id"]));
+  }
+}
