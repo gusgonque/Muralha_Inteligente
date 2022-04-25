c62ab977ad0c0499aa690e97c51e3c6619b01773
Lab_Iot <gustavoguguqueiroz@hotmail.com>
2022-04-25 16:36:40 -0300

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,42 @@
+import 'dart:js';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:muralha_inteligente_app/screens/dashboard.dart';
 import 'notifications/notifications.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
+
+void onDidReceiveLocalNotification(
+    int id, String title, String body, String payload) async {
+  // display a dialog with the notification details, tap ok to go to another page
+  showDialog(
+    context: context,
+    builder: (BuildContext context) => CupertinoAlertDialog(
+      title: Text(title),
+      content: Text(body),
+      actions: [
+        CupertinoDialogAction(
+          isDefaultAction: true,
+          child: Text('Ok'),
+          onPressed: () async {
+            Navigator.of(context, rootNavigator: true).pop();
+            await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SecondScreen(payload),
+              ),
+            );
+          },
+        )
+      ],
+    ),
+  );
+}
 
 
 void main() async{
@@ -17,6 +48,21 @@ void main() async{
   NotificationSettings settings = await messaging.requestPermission(
     sound: false,
   );
+
+
+  const AndroidInitializationSettings initializationSettingsAndroid =
+  AndroidInitializationSettings('app_icon');
+  final IOSInitializationSettings initializationSettingsIOS =
+  IOSInitializationSettings(
+      onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+  final MacOSInitializationSettings initializationSettingsMacOS =
+  MacOSInitializationSettings();
+  final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS,
+  );
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: selectNotification);
 
   if (settings.authorizationStatus == AuthorizationStatus.authorized || settings.authorizationStatus == AuthorizationStatus.provisional) {
     print('User granted permission');
--- lib/notifications/notifications.dart
+++ lib/notifications/notifications.dart
@@ -4,6 +4,8 @@ import 'package:geolocator/geolocator.dart';
 
 FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 FlutterLocalNotificationsPlugin();
+// initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+
 
 Future<void> startNotificationHandler(FirebaseMessaging messaging) async {
   String? token = await messaging.getToken();
