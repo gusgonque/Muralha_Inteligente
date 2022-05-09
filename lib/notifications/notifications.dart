a8497885866ba537953d646d7b8d25bd2dd76ec5
Lab_Iot <gustavoguguqueiroz@hotmail.com>
2022-05-09 13:19:48 -0300

    Changes

--- lib/notifications/notifications.dart
+++ lib/notifications/notifications.dart
@@ -1,5 +1,5 @@
-import 'dart:convert';
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -15,13 +15,17 @@ Future<void> startNotificationHandler(FirebaseMessaging messaging) async {
 }
 
 Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message: ${message.messageId}");
+
+  await Firebase.initializeApp();
   Position? position = await Geolocator.getLastKnownPosition();
 
   double distance = Geolocator.distanceBetween(position?.latitude ?? 0, position?.longitude ?? 0, double.parse(message.data["lat"]), double.parse(message.data["long"]));
   var distanceInKm = (distance / 1000);
   print('Distance is: $distanceInKm');
 
-  print("Handling a background message: ${message.messageId}");
-  AwesomeNotifications().createNotificationFromJsonData(message.data);
-
+  if(distanceInKm<=5)
+    AwesomeNotifications().createNotificationFromJsonData(message.data);
+  else
+    print('Não foi notificado');
 }
