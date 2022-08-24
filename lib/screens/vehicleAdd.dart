482590e4866ba004c98f19646d37620398f48242
Lab_Iot <gustavoguguqueiroz@hotmail.com>
2022-08-24 10:09:37 -0300

    24.08

--- lib/screens/vehicleAdd.dart
+++ lib/screens/vehicleAdd.dart
@@ -1,59 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class VehicleAdd extends StatefulWidget {
-
   @override
   _VehicleAddState createState() => _VehicleAddState();
 }
 
 class _VehicleAddState extends State<VehicleAdd> {
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _plateController = TextEditingController();
-
   final TextEditingController _descriptionController = TextEditingController();
+
+  static const _errorMessage = 'Campo inválido.';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Adicionar veículo suspeito'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: _plateController,
-              decoration: InputDecoration(
-                labelText: 'Placa',
-              ),
-              style: TextStyle(fontSize: 24.0),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: TextField(
-                controller: _descriptionController,
-                decoration: InputDecoration(
-                  labelText: 'Descrição do veículo',
-                ),
-                style: TextStyle(fontSize: 24.0),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 16.0),
-              child: SizedBox(
-                width: double.maxFinite,
-                child:
-                ElevatedButton(onPressed: () {
-                  final String plate = _plateController.text;
-                  final String description = _descriptionController.text;
-                  //TODO: localização
-                  //TODO: método para adicionar o veículo
-                }, child: Text('Adicionar')),
-              ),
-            )
-          ],
+        appBar: AppBar(
+          title: Text('Adicionar veículo suspeito'),
         ),
-      ),
-    );
+        body: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Placa',
+                    ),
+                    controller: _plateController,
+                    style: TextStyle(fontSize: 24.0),
+                    validator: (_textValidator) {
+                      if (_textValidator == null || _textValidator.isEmpty) {
+                        return _errorMessage;
+                      }
+                      return null;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Descrição do veículo',
+                      ),
+                      controller: _descriptionController,
+                      style: TextStyle(fontSize: 24.0),
+                      validator: (deviceName) {
+                        if (deviceName == null || deviceName.isEmpty) {
+                          return _errorMessage;
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Text(
+                      'Será usada a localização atual desse dispositivo.',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: SizedBox(
+                      width: double.maxFinite,
+                      child: ElevatedButton(
+                          onPressed: () async {
+
+                            Position position = await Geolocator.getCurrentPosition();
+
+                            if (_formKey.currentState!.validate()) {
+                              final String plate = _plateController.text;
+                              final String description = _descriptionController.text;
+
+                              //TODO: método para adicionar o veículo
+                              //todo: Navigator.pop(context);
+                            }
+                          },
+                          child: Text('Adicionar')),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
